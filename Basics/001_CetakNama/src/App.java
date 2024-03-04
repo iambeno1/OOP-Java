f17cfc5c402f7e584bf7f0384fb32991fb3e036e
iambeno1 <benonygabriel40@gmail.com>
2024-03-05 05:32:46 +0700

    belajar dasar java

---
+++ Basics/001_CetakNama/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Pembelajaran OOP Java");
+        System.out.println("Nama: Benony Gabriel");
+        System.out.println("NIM: 105222002");
+        System.out.println("Kelas: Object Oriented Programming");
+        System.out.println("Motivasi: \"Ada Usaha, pasti ada hasil\"");
+    }
+}
