4ede77ed5956f4cf122defe0d2521706dd47a0d8
iambeno1 <benonygabriel40@gmail.com>
2024-03-07 10:08:33 +0700

    java variabels

--- Basics/002_Data Types/src/_1_Variabel.java
+++ Basics/002_Data Types/src/_1_Variabel.java
@@ -13,5 +13,31 @@ public class _1_Variabel {
         // Final / const: tipe yang value nya tidak bisa dimodif/diubah
         final float PHI = 3.14f; // 'f' artinya float
         System.out.println("Nilai phi = "+ PHI);
+
+        String firstName = "Benony ";
+        String lastName = "Gabriel";
+        String fullName = firstName + lastName;
+        System.out.println("My name is "+fullName);
+
+
+        int x = 3;
+        int y = 4;
+        int kali = x * y;
+        System.out.println("x = "+x);
+        System.out.println("y = "+y);
+        System.out.println("x * y = "+kali);
+
+        System.out.println(6 * 5);
+        System.out.println("My age is "+(2024 - 2004));
+
+        // Deklarasi multi
+        int a = 2, b = 5, c = 2;
+        System.out.println(a + b + c);
+
+        int k, l, m;
+        k = l = m = 10;
+        System.out.println(k);
+        System.out.println(l);
+        System.out.println(m);
     }
 }
