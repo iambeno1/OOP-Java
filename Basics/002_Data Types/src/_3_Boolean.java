912efe5cfa296a90d0706222936442ee54cc3a5a
iambeno1 <benonygabriel40@gmail.com>
2024-03-07 10:35:02 +0700

    boolean types

--- Basics/002_Data Types/src/_3_Boolean.java
+++ Basics/002_Data Types/src/_3_Boolean.java
@@ -1,5 +1,18 @@
 public class _3_Boolean {
     public static void main(String[] args) {
-        
+        // Tipe Boolean
+        /*Sering kali dalam pemrograman, Anda memerlukan tipe data yang hanya dapat memiliki satu dari dua nilai, seperti:
+
+        YES / NO
+        ON / OFF
+        TRUE / FALSE
+        Untuk hal ini, Java memiliki tipe data boolean, yang hanya bisa menerima nilai benar atau salah: */
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+        System.out.println(!isJavaFun); // output false karena pake not / '!'
     }
 }
