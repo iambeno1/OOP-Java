7ffd04e64d87ff5e192043572c2a150380af6a95
iambeno1 <benonygabriel40@gmail.com>
2024-03-16 20:48:02 +0700

    test tukar nilai variabel

---
+++ Basics/src/_09_TukarNilai.java
@@ -0,0 +1,18 @@
+public class _09_TukarNilai {
+    public static void main(String[] args) {
+        int tiga = 3;
+        int lima = 5;
+
+        System.out.println("Nilai awal");
+        System.out.println("Tiga = " + tiga);
+        System.out.println("Lima = " + lima);
+        
+        System.out.println("\nSetelah ditukar");
+        int temp;
+        temp = tiga;
+        tiga = lima;
+        lima = temp;
+        System.out.println("Tiga = " + tiga);
+        System.out.println("Lima = " + lima);
+    }
+}
