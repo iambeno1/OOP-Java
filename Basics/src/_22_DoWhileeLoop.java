310a42b119cf5564ca14043d36577aa7db0c1f4e
iambeno1 <benonygabriel40@gmail.com>
2024-03-16 21:18:17 +0700

    loop java == C++ loop

---
+++ Basics/src/_22_DoWhileeLoop.java
@@ -0,0 +1,9 @@
+public class _22_DoWhileeLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        do{
+            System.out.println("Loop ke-" + i);
+            i++;
+        }while(i <= 10);
+    }
+}
