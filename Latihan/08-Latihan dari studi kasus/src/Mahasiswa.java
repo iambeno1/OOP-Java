df090d26710abc873ad4b3316fb1e44e726ce9e0
iambeno1 <benonygabriel40@gmail.com>
2024-04-12 00:30:04 +0700

    selesai buat kelas Mahasiswa

--- Latihan/08-Latihan dari studi kasus/src/Mahasiswa.java
+++ Latihan/08-Latihan dari studi kasus/src/Mahasiswa.java
@@ -1,47 +1,29 @@
 public class Mahasiswa {
     private String NIM;
-    private String nama;
+    private String name;
     private String asalDaerah;
 
-    public Mahasiswa(String NIM, String nama, String asalDaerah){
+    public Mahasiswa(String NIM, String name, String asalDaerah){
         this.NIM = NIM;
-        this.nama = nama;
+        this.name = name;
         this.asalDaerah = asalDaerah;
-    }
-
-    public void mainMenu(){
-        System.out.println("========== Menu Utama ==========");
-        System.out.println("1. Tambah data");
-        System.out.println("2. Lihat data");
-        System.out.println("3. Cari data pake NIM");
-        System.out.println("3. Cari data pake alamat");
-    }
-
-    public void displayData(){
-        System.out.println();
-    }
-
-    public void setNIM(String NIM){
-        this.NIM = NIM;
     }
 
     public String getNIM(){
         return NIM;
     }
 
-    public void setNama(String nama){
-        this.nama = nama;
-    }
-
-    public String getNama(){
-        return nama;
-    }
-
-    public void setAsalDaerah(String asalDaerah){
-        this.asalDaerah = asalDaerah;
+    public String getName(){
+        return name;
     }
 
     public String getAsalDaerah(){
         return asalDaerah;
+    }
+
+    public String display(){
+        return "Nama \t: " + name + "\n" +
+                "NIM \t: " + NIM + "\n" + 
+                "Asal\t: " + asalDaerah + "\n";
     }
 }
