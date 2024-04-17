6c525b294de5b9816abaaf55ec41bb1b4f946fb8
iambeno1 <benonygabriel40@gmail.com>
2024-04-18 01:36:35 +0700

    solo coding is fun

--- Latihan/10 - Solo Coding/src/Dosen/Dosen.java
+++ Latihan/10 - Solo Coding/src/Dosen/Dosen.java
@@ -1,5 +1,43 @@
 package Dosen;
 
+import java.util.ArrayList;
+
+import Mahasiswa.Mahasiswa;
+
 public class Dosen {
-    
+    private String nama;
+    private ArrayList<Mahasiswa> mahasiswas;
+
+    public void anakWali(Mahasiswa mahasiswa){
+        if(mahasiswas == null){
+            mahasiswas = new ArrayList<>();
+        }
+        mahasiswas.add(mahasiswa);
+    }
+
+    public void displayAnkWali(){
+        System.out.println("Nama Dosen \t: " + nama);
+        System.out.println("Daftar anak wali");
+        for(Mahasiswa mhs : mahasiswas){
+            System.out.println(mhs.getNama());
+            System.out.println(mhs.getNIM());
+            System.out.println();
+        }
+    }
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setMahasiswas(ArrayList<Mahasiswa> mahasiswas) {
+        this.mahasiswas = mahasiswas;
+    }
+
+    public ArrayList<Mahasiswa> getMahasiswas() {
+        return mahasiswas;
+    }
 }
