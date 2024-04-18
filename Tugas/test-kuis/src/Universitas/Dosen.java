40c2cf0bc24a28a7eeaab7bcb41493cba11034a6
iambeno1 <benonygabriel40@gmail.com>
2024-04-18 12:39:33 +0700

    kuis 1 is easyyy

---
+++ Tugas/test-kuis/src/Universitas/Dosen.java
@@ -0,0 +1,17 @@
+package Universitas;
+
+public class Dosen {
+    private String namaDosen;
+
+    public Dosen(String namaDosen) {
+        this.namaDosen = namaDosen;
+    }
+
+    public void setNamaDosen(String namaDosen) {
+        this.namaDosen = namaDosen;
+    }
+
+    public String getNamaDosen() {
+        return namaDosen;
+    }
+}
