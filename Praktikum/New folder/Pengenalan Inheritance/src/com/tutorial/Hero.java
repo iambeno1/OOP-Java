6329517f8fe371c7754074324854402f0d976af7
iambeno1 <benonygabriel40@gmail.com>
2024-05-17 15:19:40 +0700

    pert 9 praktikum

---
+++ Praktikum/New folder/Pengenalan Inheritance/src/com/tutorial/Hero.java
@@ -0,0 +1,11 @@
+package com.tutorial;
+
+// superclass, parent class, base class
+public class Hero {
+    String name;
+
+    void display(){
+        System.out.println("Name : " + name);
+    }
+    
+}
